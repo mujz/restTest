1da28dab0deb21fd96ac9e4a091badadb6160a2b
Mujtaba Al-Tameemi <mujtaba.altameemi@gmail.com>
2017-06-07 15:33:33 -0700

    documented errors.go

--- errors.go
+++ errors.go
@@ -2,11 +2,15 @@ package restTest
 
 import "fmt"
 
+// Returned when a remote server responds with a non-200 status code.
 type HTTPError struct {
-	Status     string
+	// Error status string. Ex. 404 Not Found. Matches http.Response.Status.
+	Status string
+	// Error status code. Ex. 404. Matches http.Response.StatusCode.
 	StatusCode int
 }
 
+// Implements error
 func (err HTTPError) Error() string {
 	return fmt.Sprintf("Remote server responded with status: %s", err.Status)
 }
